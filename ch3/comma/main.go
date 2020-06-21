93c7de24840126a35577ddbe3b665bf62f8dcb8a
jhamPac <cloud@kevia.me>
2020-06-21 10:06:08 -0700

    simple comma function

---
+++ ch3/comma/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+func main() {
+	billion := "1000000000"
+	b := comma(billion)
+	fmt.Println(b)
+}
