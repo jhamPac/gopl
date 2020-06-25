6816bcd564c0e36223d0ef1bf4ea5a0cd3e15f6c
jhamPac <cloud@kevia.me>
2020-06-25 07:32:29 -0700

    moved main.go into its own cmd directory

---
+++ ch4/issuefetcher/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jhampac/gopl/ch4/issuefetcher"
+)
+
+func main() {
+	result, err := issuefetcher.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
+}
