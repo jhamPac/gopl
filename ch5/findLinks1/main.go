8aaedb209b621a1f412c7b130f27b1acf5788e73
jhamPac <cloud@kevia.me>
2020-06-29 07:08:17 -0700

    changed fl to not use a loop

--- ch5/findLinks1/main.go
+++ ch5/findLinks1/main.go
@@ -13,21 +13,18 @@ func main() {
 		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
 		os.Exit(1)
 	}
-	for _, link := range visit(nil, doc) {
-		fmt.Println(link)
-	}
+	visit(doc)
 }
 
-func visit(links []string, n *html.Node) []string {
+func visit(n *html.Node) {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, a := range n.Attr {
 			if a.Key == "href" {
-				links = append(links, a.Val)
+				fmt.Println(a.Val)
 			}
 		}
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		links = visit(links, c)
+		visit(c)
 	}
-	return links
 }
