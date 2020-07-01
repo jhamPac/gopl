15d71ed138dc7e1167d817513b563243c2d201d8
jhamPac <cloud@kevia.me>
2020-06-30 18:13:45 -0700

    created a custom type for html prettier funcs

---
+++ ch5/prettyhtml/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+type htmlPrettier func(n *html.Node)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		outline(url)
+	}
+}
+
+func outline(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	forEachNode(doc, startElement, endElement)
+	return nil
+}
+
+// forEachNode is like a map where you pass in callbacks point free style
+func forEachNode(n *html.Node, pre, post htmlPrettier) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
