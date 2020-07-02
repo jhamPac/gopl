695737ccf3e5790251028b480f4384c761fd4c84
jhamPac <cloud@kevia.me>
2020-07-02 11:10:42 -0700

    ready to write topoSort

---
+++ ch5/ex5.11/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var prereqs = map[string][]string{
+	"algorithms":           {"data structures"},
+	"calculus":             {"linear algebra"},
+	"linear algebra":       {"calculus"},
+	"intro to programming": {"data structures"},
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func main() {
+	order, err := topoSort(prereqs)
+	if err != nil {
+		fmt.Println(os.Stderr, err)
+		os.Exit(1)
+	}
+	for i, course := range order {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
+}
+
+func index(s string, slice []string) (int, error) {
+	for i, v := range slice {
+		if s == v {
+			return i, nil
+		}
+	}
+	return 0, fmt.Errorf("not found")
+}
