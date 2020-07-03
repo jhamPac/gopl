062e091e9131a594a604408516c22583d76453bb
jhamPac <cloud@kevia.me>
2020-07-03 11:40:00 -0700

    IntSet practice

---
+++ ch6/intset/main.go
@@ -0,0 +1,12 @@
+package main
+
+// IntSet is a set of small non-negative integers
+type IntSet struct {
+	words []uint64
+}
+
+// Has reports whether the set contains the non-negative value x
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
