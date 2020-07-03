65ed46314f6d620ec14c68442d0c6a2fe97c34f1
jhamPac <cloud@kevia.me>
2020-07-03 12:02:41 -0700

    a String method for IntSet

--- ch6/intset/main.go
+++ ch6/intset/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+)
+
 // IntSet is a set of small non-negative integers
 type IntSet struct {
 	words []uint64
@@ -29,4 +34,25 @@ func (s *IntSet) UnionWith(t *IntSet) {
 			s.words = append(s.words, tword)
 		}
 	}
+}
+
+// String returns the set as a string of the form "{1 2 3}"
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
 }
