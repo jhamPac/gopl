6edc2915ff3c57fe51fbb79f12b463621253b339
jhamPac <cloud@kevia.me>
2020-07-03 18:43:53 -0700

    second test pass

--- ch7/counter/counter_test.go
+++ ch7/counter/counter_test.go
@@ -18,3 +18,24 @@ func TestLineCounter(t *testing.T) {
 		t.Logf("lines: %d != 3", c.N())
 	}
 }
+
+func TestWordCounter(t *testing.T) {
+	c := &WordCounter{}
+	data := [][]byte{
+		[]byte("The upcoming word is sp"),
+		[]byte("lit across the buffer boundary. "),
+		[]byte(" And this one ends on the buffer boundary."),
+		[]byte(" Last words."),
+	}
+	for _, p := range data {
+		n, err := c.Write(p)
+		if n != len(p) || err != nil {
+			t.Logf(`bad write: p="%s" n=%d err="%s"`, string(p), n, err)
+			t.Fail()
+		}
+	}
+	if c.N() != 19 {
+		t.Logf("words: %d != 19", c.N())
+		t.Fail()
+	}
+}
