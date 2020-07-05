169d832f3686874881db455be1632ad8018ca5ab
jhamPac <cloud@kevia.me>
2020-07-05 13:22:16 -0700

    update ByArtist so it is exported

---
+++ ch7/shazam/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"sort"
+
+	"github.com/jhampac/gopl/ch7/shazam/music"
+)
+
+func main() {
+	tracks := []*music.Track{
+		music.NewTrack("Go", "Delilah", "From the Roots Up", 2012, "3m38s"),
+		music.NewTrack("Go", "Moby", "Moby Play", 1992, "3m10s"),
+		music.NewTrack("Go Ahead", "Alicia Keys", "As Iam", 2007, "4m36s"),
+		music.NewTrack("Read 2 Go", "Martin Solveig", "Samsh", 2011, "4m00s"),
+	}
+
+	sort.Sort(sorting.ByArtist(tracks))
+}
--- ch7/shazam/sorting/sorting.go
+++ ch7/shazam/sorting/sorting.go
@@ -4,16 +4,17 @@ import (
 	"github.com/jhampac/gopl/ch7/shazam/music"
 )
 
-type byArtist []*music.Track
+// ByArtist sorts a slice of Track by artist
+type ByArtist []*music.Track
 
-func (x byArtist) Len() int {
+func (x ByArtist) Len() int {
 	return len(x)
 }
 
-func (x byArtist) Less(i, j int) bool {
+func (x ByArtist) Less(i, j int) bool {
 	return x[i].Artist < x[j].Artist
 }
 
-func (x byArtist) Swap(i, j int) {
+func (x ByArtist) Swap(i, j int) {
 	x[i], x[j] = x[j], x[i]
 }
