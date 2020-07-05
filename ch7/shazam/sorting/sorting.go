46e1a98895c51743be5cc7365b2715fcca9b5bcf
jhamPac <cloud@kevia.me>
2020-07-05 13:16:31 -0700

    sorting implements the sort.Interface

--- ch7/shazam/sorting/sorting.go
+++ ch7/shazam/sorting/sorting.go
@@ -1,1 +1,19 @@
 package sorting
+
+import (
+	"github.com/jhampac/gopl/ch7/shazam/music"
+)
+
+type byArtist []*music.Track
+
+func (x byArtist) Len() int {
+	return len(x)
+}
+
+func (x byArtist) Less(i, j int) bool {
+	return x[i].Artist < x[j].Artist
+}
+
+func (x byArtist) Swap(i, j int) {
+	x[i], x[j] = x[j], x[i]
+}
