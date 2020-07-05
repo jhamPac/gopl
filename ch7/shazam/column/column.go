d8e779a36877bc86e60721fe1b1c59d458b2554b
jhamPac <cloud@kevia.me>
2020-07-05 13:50:42 -0700

    done with column; not 100% sure what is going on but seems legit

--- ch7/shazam/column/column.go
+++ ch7/shazam/column/column.go
@@ -33,3 +33,80 @@ type ByColumns struct {
 func NewByColumns(p []Person, maxColumns int) *ByColumns {
 	return &ByColumns{p, nil, maxColumns}
 }
+
+// LessName compares names
+func (c *ByColumns) LessName(a, b *Person) comparison {
+	switch {
+	case a.Name == b.Name:
+		return eq
+	case a.Name < b.Name:
+		return lt
+	default:
+		return gt
+	}
+}
+
+// LessSumOfAgeDigits compares ages
+func (c *ByColumns) LessSumOfAgeDigits(a, b *Person) comparison {
+	aSum := sumOfDigits(a.Age)
+	bSum := sumOfDigits(b.Age)
+	switch {
+	case aSum == bSum:
+		return eq
+	case aSum < bSum:
+		return lt
+	default:
+		return gt
+	}
+}
+
+func sumOfDigits(n int) int {
+	sum := 0
+	for ; n > 0; n /= 10 {
+		sum += n % 10
+	}
+	return sum
+}
+
+// LessAge compares less age
+func (c *ByColumns) LessAge(a, b *Person) comparison {
+	switch {
+	case a.Age == b.Age:
+		return eq
+	case a.Age < b.Age:
+		return lt
+	default:
+		return gt
+	}
+}
+
+func (c *ByColumns) Len() int {
+	return len(c.p)
+}
+
+func (c *ByColumns) Swap(i, j int) {
+	c.p[i], c.p[j] = c.p[j], c.p[i]
+}
+
+func (c *ByColumns) Less(i, j int) bool {
+	for _, f := range c.columns {
+		cmp := f(&c.p[i], &c.p[j])
+		switch cmp {
+		case eq:
+			continue
+		case lt:
+			return true
+		case gt:
+			return false
+		}
+	}
+	return false
+}
+
+func (c *ByColumns) Select(cmp columnCmp) {
+	c.columns = append([]columnCmp{cmp}, c.columns...)
+
+	if len(c.columns) > c.maxColumns {
+		c.columns = c.columns[:c.maxColumns]
+	}
+}
