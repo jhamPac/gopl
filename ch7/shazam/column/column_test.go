d20f07be33610d2d2f605f52ef4dd2c77661a929
jhamPac <cloud@kevia.me>
2020-07-05 13:56:59 -0700

    first test passed

---
+++ ch7/shazam/column/column_test.go
@@ -0,0 +1,37 @@
+package column
+
+import (
+	"sort"
+	"testing"
+)
+
+func Cmp(a, b []Person, t *testing.T) {
+	if len(a) != len(b) {
+		t.Log("different lengths")
+		t.Logf("%s\n%s", a, b)
+		t.Fail()
+		return
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			t.Logf("different elements, starting at %d", i)
+			t.Logf("%s\n%s", a, b)
+			t.Fail()
+			return
+		}
+	}
+}
+
+func TestByColumns_Age(t *testing.T) {
+	people := []Person{
+		{"Alice", 20},
+		{"Alice", 12},
+	}
+	c := &ByColumns{people, nil, 2}
+	c.Select(c.LessAge)
+	sort.Sort(c)
+	Cmp(people, []Person{
+		{"Alice", 12},
+		{"Alice", 20},
+	}, t)
+}
