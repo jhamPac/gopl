6f3630b0acc384a01f8ade31f0891567e8dd6ec4
jhamPac <cloud@kevia.me>
2020-07-04 22:53:47 -0700

    added degree symbol in comments

--- ch7/tempflag/main.go
+++ ch7/tempflag/main.go
@@ -2,26 +2,26 @@ package main
 
 import "fmt"
 
-// Celsius represents C
+// Celsius represents °C
 type Celsius float64
 
-// Fahrenheit represents F
+// Fahrenheit represents °F
 type Fahrenheit float64
 
-// Kelvin represents K
+// Kelvin represents °K
 type Kelvin float64
 
-// CToF converts C to F
+// CToF converts °C to °F
 func CToF(c Celsius) Fahrenheit {
 	return Fahrenheit(c*9.0/5.0 + 32.0)
 }
 
-// FToC converts F to C
+// FToC converts °F to °C
 func FToC(f Fahrenheit) Celsius {
 	return Celsius((f - 32.0) * 5.0 / 9.0)
 }
 
-// KToC converts K to C
+// KToC converts °K to °C
 func KToC(k Kelvin) Celsius {
 	return Celsius(k - 273.15)
 }
