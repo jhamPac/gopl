41f4beb022f0d9a4c61a8a3ce77878dc8b5d44cf
jhamPac <cloud@kevia.me>
2020-07-06 09:08:00 -0700

    code for a lexer

---
+++ ch7/webcalculator/eval/parse.go
@@ -0,0 +1,43 @@
+package eval
+
+import (
+	"fmt"
+	"text/scanner"
+)
+
+type lexer struct {
+	scan  scanner.Scanner
+	token rune
+}
+
+func (lex *lexer) next() {
+	lex.token = lex.scan.Scan()
+}
+
+func (lex *lexer) text() string {
+	return lex.scan.TokenText()
+}
+
+type lexPanic string
+
+func (lex *lexer) describe() string {
+	switch lex.token {
+	case scanner.EOF:
+		return "end of file"
+	case scanner.Ident:
+		return fmt.Sprintf("identifier %s", lex.text())
+	case scanner.Int, scanner.Float:
+		return fmt.Sprintf("number %s", lex.text())
+	}
+	return fmt.Sprintf("%q", rune(lex.token))
+}
+
+func precedence(op rune) int {
+	switch op {
+	case '*', '/':
+		return 2
+	case '+', '-':
+		return 1
+	}
+	return 0
+}
