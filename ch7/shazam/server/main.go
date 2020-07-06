0d18dc3d29982c033c11994c9e84f96ae7cfb3c5
jhamPac <cloud@kevia.me>
2020-07-05 17:23:28 -0700

    a simple server that sorts a table of tracks

--- ch7/shazam/server/main.go
+++ ch7/shazam/server/main.go
@@ -1,6 +1,14 @@
 package main
 
-import "github.com/jhampac/gopl/ch7/shazam/music"
+import (
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"sort"
+
+	"github.com/jhampac/gopl/ch7/shazam/music"
+)
 
 type clickedSort struct {
 	tracks []*music.Track
@@ -17,4 +25,87 @@ func (x clickedSort) Less(i, j int) bool {
 
 func (x clickedSort) Swap(i, j int) {
 	x.tracks[i], x.tracks[j] = x.tracks[j], x.tracks[i]
+}
+
+var tracks = []*music.Track{
+	music.NewTrack("Go", "Delilah", "From the Roots Up", 2012, "3m38s"),
+	music.NewTrack("Go", "Moby", "Moby Play", 1992, "3m10s"),
+	music.NewTrack("Go Ahead", "Alicia Keys", "As Iam", 2007, "4m36s"),
+	music.NewTrack("Read 2 Go", "Martin Solveig", "Samsh", 2011, "4m00s"),
+}
+
+var trackTable = template.Must(template.New("trackTable").Parse(`
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	  <meta charset="utf-8">
+		<style media="screen" type="text/css">
+		  table {
+				border-collapse: collapse;
+				border-spacing: 0px;
+			}
+		  table, th, td {
+				padding: 5px;
+				border: 1px solid black;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Tracks</h1>
+		<table>
+		  <thead>
+				<tr>
+					<th><a href="/?sort=Title">Title</a></th>
+					<th><a href="/?sort=Artist">Artist</a></th>
+					<th><a href="/?sort=Album">Album</a></th>
+					<th><a href="/?sort=Year">Year</a></th>
+					<th><a href="/?sort=Length">Length</a></th>
+				</tr>
+			</thead>
+			<tbody>
+				{{range .}}
+				<tr>
+					<td>{{.Title}}</td>
+					<td>{{.Artist}}</td>
+					<td>{{.Album}}</td>
+					<td>{{.Year}}</td>
+					<td>{{.Length}}</td>
+				</tr>
+				{{end}}
+			</tbody>
+		</table>
+	</body>
+</html>
+`))
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:9000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	sortBy := r.FormValue("sort")
+
+	sort.Sort(clickedSort{tracks, func(track1, track2 *music.Track) bool {
+		switch sortBy {
+		case "Title":
+			return track1.Title < track2.Title
+		case "Year":
+			return track1.Year < track2.Year
+		case "Length":
+			return track1.Length < track2.Length
+		case "Artist":
+			return track1.Artist < track2.Artist
+		case "Album":
+			return track1.Album < track2.Album
+		}
+		return false
+	}})
+	printTracks(w, tracks)
+}
+
+func printTracks(w io.Writer, tracks []*music.Track) {
+	if err := trackTable.Execute(w, tracks); err != nil {
+		log.Fatal(err)
+	}
 }
