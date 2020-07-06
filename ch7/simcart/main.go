bc4e413881c167e39fb944ff0a9c74a1bc86f556
jhamPac <cloud@kevia.me>
2020-07-05 17:56:08 -0700

    create route done

--- ch7/simcart/main.go
+++ ch7/simcart/main.go
@@ -1,6 +1,14 @@
 package main
 
-var listHTML = template.Must(template.New("simcart").Parse(`
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+var listHTML = template.Must(template.New("list").Parse(`
 <html>
 <body>
 <table>
@@ -18,3 +26,37 @@ var listHTML = template.Must(template.New("simcart").Parse(`
 </body>
 </html>
 `))
+
+// PriceDB is the main database that houses all the items and prices
+type PriceDB struct {
+	sync.Mutex
+	db map[string]int
+}
+
+// Create an item in the database
+func (p *PriceDB) Create(w http.ResponseWriter, r *http.Request) {
+	item := r.FormValue("item")
+	if item == "" {
+		http.Error(w, "no item given", http.StatusBadRequest)
+		return
+	}
+
+	priceStr := r.FormValue("price")
+	price, err := strconv.Atoi(priceStr)
+	if err != nil {
+		http.Error(w, "no integer price given", http.StatusBadRequest)
+		return
+	}
+
+	if _, ok := p.db[item]; ok {
+		http.Error(w, fmt.Sprintf("%s already exists", item), http.StatusBadRequest)
+		return
+	}
+
+	p.Lock()
+	if p.db == nil {
+		p.db = make(map[string]int, 0)
+	}
+	p.db[item] = price
+	p.Unlock()
+}
