5541271f67a06597d424d30df1c1ea7a458aa57e
jhamPac <cloud@kevia.me>
2020-07-13 19:53:36 -0700

    started work for the reverb

---
+++ ch8/reverb/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration, wg *sync.WaitGroup) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+	wg.Done()
+}
