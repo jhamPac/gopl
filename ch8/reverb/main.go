b0022dcdf2820358009a1a2777ecfd9c5f9c5a01
jhamPac <cloud@kevia.me>
2020-07-13 20:04:27 -0700

    reverb echo server done

--- ch8/reverb/main.go
+++ ch8/reverb/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"strings"
 	"sync"
@@ -15,4 +17,30 @@ func echo(c net.Conn, shout string, delay time.Duration, wg *sync.WaitGroup) {
 	time.Sleep(delay)
 	fmt.Fprintln(c, "\t", strings.ToLower(shout))
 	wg.Done()
+}
+
+func handleConn(c net.Conn) {
+	var wg sync.WaitGroup
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		wg.Add(1)
+		go echo(c, input.Text(), 1*time.Second, &wg)
+	}
+	wg.Wait()
+	c.Close()
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:9000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
 }
