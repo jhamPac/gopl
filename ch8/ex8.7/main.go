7b37965f2bd6d467cf8635c36ce682b92bcaf184
jhamPac <cloud@kevia.me>
2020-07-14 20:29:12 -0700

    save point

---
+++ ch8/ex8.7/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+
+	"golang.org/x/net/html"
+)
+
+var tokens = make(chan struct{}, 20)
+var maxDepth int
+var seen = make(map[string]bool)
+var seenLock = sync.Mutex{}
+var base *url.URL
+
+func crawl(url string, depth int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	tokens <- struct{}{}
+	urls, err := visit(url)
+	if err != nil {
+		log.Printf("visit %s: %s", url, err)
+	}
+
+	if depth >= maxDepth {
+		return
+	}
+}
+
+func visit(rawURL string) (url []string, err error) {
+	fmt.Println(rawURL)
+	resp, err := http.Get(rawURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("GET %s: %s", rawURL, resp.Status)
+	}
+
+	u, err := base.Parse(rawURL)
+	if err != nil {
+		return nil, err
+	}
+	if base.Host != u.Host {
+		log.Printf("not saving %s: non-local", rawURL)
+		return nil, nil
+	}
+
+	var body io.Reader
+	contentType := resp.Header["Content-Type"]
+	if strings.Contains(strings.Join(contentType, ","), "text/html") {
+		doc, err := html.Parse(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			return nil, fmt.Errorf("parsing %s as HTML: %v", u, err)
+		}
+		nodes := linkNodes(doc)
+		urls = linkURLs(nodes, u)
+		rewriteLocalLinks(nodes, u)
+		b := &bytes.Buffer{}
+		err = html.Render(b, doc)
+		if err != nil {
+			log.Printf("render %s: %s", u, err)
+		}
+		body = b
+	}
+	err = save(resp, body)
+	return urls, err
+}
+
+func linkNodes(n *html.Node) []*html.Node {
+	var links []*html.Node
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			links = append(links, n)
+		}
+	}
+	forEachNode(n, visitNode, nil)
+	return links
+}
