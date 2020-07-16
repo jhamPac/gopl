a93cf3ca333c94b7f670b3b64d03cbb03abcb788
jhamPac <cloud@kevia.me>
2020-07-16 07:02:16 -0700

    simple chat server done

--- ch8/chat/main.go
+++ ch8/chat/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
 )
@@ -18,5 +20,59 @@ func main() {
 			continue
 		}
 		go handleConn(conn)
+	}
+}
+
+// outgoing message channel
+type client chan<- string
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string)
+)
+
+func broadcaster() {
+	clients := make(map[client]bool)
+	for {
+		select {
+		case msg := <-messages:
+			// broadcast message to all clients
+			for c := range clients {
+				c <- msg
+			}
+
+		case c := <-entering:
+			clients[c] = true
+
+		case c := <-leaving:
+			delete(clients, c)
+			close(c)
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	outgoingMsg := make(chan string)
+	go clientWriter(conn, outgoingMsg)
+
+	who := conn.RemoteAddr().String()
+	outgoingMsg <- "client[ID] " + who
+	messages <- who + " has entered the chat room"
+	entering <- outgoingMsg
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+	}
+
+	leaving <- outgoingMsg
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg)
 	}
 }
