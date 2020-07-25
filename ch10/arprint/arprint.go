31f29302a6f095b9cae8a291eddabb726493ab0f
jhamPac <cloud@kevia.me>
2020-07-24 18:56:51 -0700

    arprint package done

--- ch10/arprint/arprint.go
+++ ch10/arprint/arprint.go
@@ -1,1 +1,49 @@
 package arprint
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+type format struct {
+	name        string
+	magic       string
+	magicOffset int
+	reader      NewReader
+}
+
+// NewReader receives an os.File type and returns a new reader
+type NewReader func(*os.File) (io.Reader, error)
+
+var formats []format
+
+// RegisterFormat appends formats to a slice
+func RegisterFormat(name, magic string, magicOffset int, f NewReader) {
+	formats = append(formats, format{name, magic, magicOffset, f})
+}
+
+// Open a file
+func Open(file *os.File) (io.Reader, error) {
+	var found *format
+	r := bufio.NewReader(file)
+	for _, f := range formats {
+		p, err := r.Peek(f.magicOffset + len(f.magic))
+		if err != nil {
+			continue
+		}
+		if string(p[f.magicOffset:]) == f.magic {
+			found = &f
+			break
+		}
+	}
+	if found == nil {
+		return nil, fmt.Errorf("open archive: can not determine format")
+	}
+	_, err := file.Seek(0, os.SEEK_SET)
+	if err != nil {
+		return nil, fmt.Errorf("open archive: %s", err)
+	}
+	return found.reader(file)
+}
