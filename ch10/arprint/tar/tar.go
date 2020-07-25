4b44297ca23a0f8be02c1f957117d3cc4fb5b047
jhamPac <cloud@kevia.me>
2020-07-25 08:43:50 -0700

    tar Read function done

--- ch10/arprint/tar/tar.go
+++ ch10/arprint/tar/tar.go
@@ -2,6 +2,7 @@ package tar
 
 import (
 	"archive/tar"
+	"io"
 	"os"
 )
 
@@ -9,4 +10,35 @@ type reader struct {
 	tarReader *tar.Reader
 	file      *os.File
 	toWrite   string
+}
+
+func (r *reader) Read(b []byte) (written int, err error) {
+	for len(b) > 0 {
+		if len(r.toWrite) > 0 {
+			n := copy(b, r.toWrite)
+			written += n
+			r.toWrite = r.toWrite[n:]
+			b = b[n:]
+		}
+		n, err := r.tarReader.Read(b)
+		written += n
+		b = b[n:]
+
+		switch err {
+		case io.EOF:
+			h, err := r.tarReader.Next()
+			if err != nil {
+				return written, err
+			}
+			if h.Typeflag == tar.TypeDir {
+				continue
+			}
+			r.toWrite = h.Name + ":\n"
+		case nil:
+			continue
+		default:
+			return written, err
+		}
+	}
+	return written, nil
 }
