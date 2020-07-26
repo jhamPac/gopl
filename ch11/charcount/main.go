777968d5d50c530e223d6005d22901cc12cc3cd3
jhamPac <cloud@kevia.me>
2020-07-26 10:16:14 -0700

    charCount main done

--- ch11/charcount/main.go
+++ ch11/charcount/main.go
@@ -2,13 +2,37 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"log"
+	"os"
 	"unicode"
 )
 
 func main() {
+	runes, props, sizes, invalid := charCount(os.Stdin)
+	fmt.Println(props)
+	fmt.Printf("rune\tcount\n")
 
+	for c, n := range runes {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range sizes {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+
+	fmt.Printf("\n%-30s count\n", "category")
+	for cat, n := range props {
+		fmt.Printf("%-30s %d\n", cat, n)
+	}
+
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
 }
 
 func charCount(r io.Reader) (
